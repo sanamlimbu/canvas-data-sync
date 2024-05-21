d4bb12e1a9ea2cf2182fc54abb50542a0e187da4
Sanam Limbu <sudosanam@gmail.com>
2024-05-21 10:07:14 +0800

    added coroutines

--- canvas.py
+++ canvas.py
@@ -1,8 +1,6 @@
 import asyncio
 import os
 from enum import StrEnum
-from .constants import CANVAS_TABLES
-
 from dap.actions.init_db import init_db
 from dap.actions.sync_db import sync_db
 from dap.api import DAPClient
@@ -13,7 +11,9 @@ import sys
 
 class SyncTableResult(StrEnum):
     INIT_NEEDED = "init_needed"
-    INIT_NOT_NEEDED = "init_not_needed"
+    COMPLETED = "completed"
+    FAILED = "failed"
+    NO_TABLE = "no_table"
 
 
 class InitTableResult(StrEnum):
@@ -21,19 +21,37 @@ class InitTableResult(StrEnum):
     FAILED = "failed"
 
 
-db_location = sys.argv[1]
-if db_location != "supabase" or db_location != "local":
-    print("Invalid argument. Enter 'supabase' or 'local'.")
+canvas_tables = [
+    "accounts",
+    "pseudonyms",
+    "users",
+    "courses",
+    "assignments",
+    "course_sections",
+    "enrollments",
+]
+
+
+if len(sys.argv) < 2:
+    print("argument missing, enter 'supabase' or 'local'.")
     exit(1)
 
-api_url = os.environ.get("DAP_API_URL")
+db_location = sys.argv[1]
+
+if db_location != "supabase" and db_location != "local":
+    print("invalid argument, enter 'supabase' or 'local'.")
+    exit(1)
+
+base_url = os.environ.get("DAP_API_URL")
 dap_client_id = os.environ.get("DAP_CLIENT_ID")
 dap_client_secret = os.environ.get("DAP_CLIENT_SECRET")
-dap_connection_string = (
+
+connection_string = (
     os.environ.get("DAP_CONNECTION_STRING_SUPABASE")
     if db_location == "supabase"
     else os.environ.get("DAP_CONNECTION_STRING_LOCAL")
 )
+
 namespace = "canvas"
 
 
@@ -42,72 +60,113 @@ async def main():
         client_id=dap_client_id, client_secret=dap_client_secret
     )
 
-    for t in CANVAS_TABLES:
-        result = await async_sync_table(
-            t, api_url, credentials, namespace, dap_connection_string
+    tasks = [
+        sync_or_init_table(
+            base_url=base_url,
+            namespace=namespace,
+            table_name=table_name,
+            credentials=credentials,
+            connection_string=connection_string,
         )
-        if result == SyncTableResult.INIT_NEEDED:
-            result == await async_init_table(
-                t, api_url, credentials, namespace, dap_connection_string
-            )
-            if result == InitTableResult.FAILED:
-                print(f"{t} init failed.")
-            else:
-                print(f"{t} init successfull.")
-        else:
-            print(f"{t} sync successfull.")
+        for table_name in canvas_tables
+    ]
+
+    await asyncio.gather(*tasks)
 
 
-async def async_get_tables(api_url: str, credentials: Credentials, namespace: str):
+async def async_get_tables(base_url: str, credentials: Credentials, namespace: str):
     async with DAPClient(
-        base_url=api_url,
+        base_url=base_url,
         credentials=credentials,
     ) as session:
         return await session.get_tables(namespace=namespace)
 
 
-async def async_sync_table(
-    table: str,
-    api_url: str,
-    credentials: Credentials,
+async def sync_table(
+    base_url: str,
     namespace: str,
+    table_name: str,
+    credentials: Credentials,
     connection_string: str,
 ):
-    result = SyncTableResult.INIT_NOT_NEEDED
+    result = SyncTableResult.COMPLETED
     try:
         await sync_db(
-            base_url=api_url,
+            base_url=base_url,
             namespace=namespace,
-            table_name=table,
+            table_name=table_name,
             credentials=credentials,
             connection_string=connection_string,
         )
+
     except NonExistingTableError as e:
-        result = SyncTableResult.INIT_NEEDED
+        result = SyncTableResult.NO_TABLE
+
+    except ValueError as e:
+        if "table not initialized" in str(e):
+            result = SyncTableResult.INIT_NEEDED
+
+    except Exception as e:
+        print(f"{table_name} sync_table exception: {e}")
+        result = SyncTableResult.FAILED
 
     return result
 
 
-async def async_init_table(
-    table: str,
-    api_url: str,
-    credentials: Credentials,
+async def init_table(
+    base_url: str,
     namespace: str,
+    table_name: str,
+    credentials: Credentials,
     connection_string: str,
 ):
     result = InitTableResult.COMPLETED
     try:
         await init_db(
-            base_url=api_url,
+            base_url=base_url,
             namespace=namespace,
-            table_name=table,
+            table_name=table_name,
             credentials=credentials,
             connection_string=connection_string,
         )
     except Exception as e:
+        print(f"{table_name} init_table exception: {e}")
         result = InitTableResult.FAILED
 
     return result
+
+
+async def sync_or_init_table(
+    base_url: str,
+    namespace: str,
+    table_name: str,
+    credentials: Credentials,
+    connection_string: str,
+):
+    try:
+        result = await sync_table(
+            base_url=base_url,
+            namespace=namespace,
+            table_name=table_name,
+            credentials=credentials,
+            connection_string=connection_string,
+        )
+
+        if result == SyncTableResult.INIT_NEEDED:
+            result = await init_table(
+                base_url=base_url,
+                namespace=namespace,
+                table_name=table_name,
+                credentials=credentials,
+                connection_string=connection_string,
+            )
+
+            print(f"{result} init table: {table_name}")
+
+        else:
+            print(f"{result} sync table: {table_name}")
+    except Exception as e:
+        print(f"{table_name} sync_or_init_table exception: {e}")
 
 
 asyncio.run(main())
